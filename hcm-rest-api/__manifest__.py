b7b1fdb2e0945dc630ae95c48cbf1c33dcab6fcd
djseif <es_djelloul@esi.dz>
2021-03-12 11:48:28 +0100

    fix verion of addons

--- hcm-rest-api/__manifest__.py
+++ hcm-rest-api/__manifest__.py
@@ -12,7 +12,7 @@
     'author': "SEIF AND KARIM",
 
     'category': 'developers',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
